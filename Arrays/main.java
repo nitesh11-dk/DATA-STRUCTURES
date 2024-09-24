3bfffe80e10f5d2a18d7e1b2390c58be38004c95
Nitesh-DK <nittesh.dk@gmail.com>
2024-09-24 22:28:05 +0530

    doen

--- Arrays/main.java
+++ Arrays/main.java
@@ -22,28 +22,41 @@ public class main{
 // }
 
 
-int arr[][] = {
-    {2, 3, 4},
-    {4, 5, 6}
-};
+// int arr[][] = {
+//     {2, 3, 4},
+//     {4, 5, 6}
+// };
 
-int arrSum[] = new int[arr.length];
+// int arrSum[] = new int[arr.length];
 
-int arrCol [] = new int[arr[0].length];
-//  0+2+4 , 0+3+5 ,0+4+6 
+// int arrCol [] = new int[arr[0].length];
+// //  0+2+4 , 0+3+5 ,0+4+6 
 
-for (int i = 0; i < arr.length; i++) {
+// for (int i = 0; i < arr.length; i++) {
 
-    for (int j =0; j <arr[i].length ;j++) {
-    arrCol[j] +=arr[i][j];
+//     for (int j =0; j <arr[i].length ;j++) {
+//     arrCol[j] +=arr[i][j];
 
-    }
+//     }
    
-}
+// }
 
-for (int i :arrCol){
-    System.out.print(i + " ");
-}
+// for (int i :arrCol){
+//     System.out.print(i + " ");
+// }
 
-    }
-}
+
+// 3d 
+// linear serach and binary search 
+
+// some question
+// getter setter
+// opps solve moreqeustion 
+
+// js 
+
+
+
+
+//     }
+// }
